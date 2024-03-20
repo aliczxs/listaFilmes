106da4a5560d3df554daeeec3920b9dadabb7336
ALICIA PATRICIA SANTOS SANTANA <alicia.pssantana@senacsp.edu.br>
2024-03-19 21:12:50 -0300

    fisrt commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const filmes = require('./filmes.json');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/filmes', (req, res) => {
+    res.json(filmes);
+});
+app.listen(PORT, () => {
+    console.log(`Servidor em execução em http://localhost:${PORT}`);
+});
+
+
+
+
